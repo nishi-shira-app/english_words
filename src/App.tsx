7767a4ce8ea0f3a734a145d882be5e5953f0c69b
Hirofumi Inoue <acliss19xx@gmail.com>
2021-05-03 10:09:29 +0900

    #1 テーマ material ui を使用し ダークモードで 実装した。

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import * as colors from "@material-ui/core/colors";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: colors.blue[800],
+      },
+      type: 'dark',
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      sample
+    </ThemeProvider>
   );
 }
 
